1121039407a485743f897e761b5918cf43c0928f
matrixpower1004 <matrixpower1004@gmail.com>
2023-06-27 06:05:05 +0900

    Add #16 - console message, error message
    기능 추가에 따른 성공, 실패 출력 메시지 추가

--- src/main/java/team/snowball/baseball/code/ErrorMessage.java
+++ src/main/java/team/snowball/baseball/code/ErrorMessage.java
@@ -13,7 +13,9 @@ import lombok.Getter;
 public enum ErrorMessage {
     ERR_MSG_INVALID_DATABASE("The database has encountered an issue. Please try again"),
     ERR_MSG_INVALID_INPUT("The input is empty or invalid. Please try again."),
-    ERR_MSG_DUPLICATE_ID("Input id is Duplicated. Please input different id.");
+    ERR_MSG_DUPLICATE_ID("Duplicate input ID. Please provide a unique ID."),
+    ERR_MSG_FAILED_TO_REGISTER("The data registration failed. Please try again."),
+    ERR_MSG_FAILED_TO_DELETE("The data deletion failed. Please try again.");
 
     private final String errorMessage;
 }
