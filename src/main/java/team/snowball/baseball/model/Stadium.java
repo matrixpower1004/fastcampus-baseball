3b66aee588dde49405a576c5e539ff84fd06bcfa
Horaiz-UQ <ekzmvlfxj@naver.com>
2023-06-26 12:53:42 +0900

    Implement_Kim #1 - Team model 생성 / Stadium model 생성
    * Team model 구현
    * Stadium model 구현
    * Builder 패턴 적용

---
+++ src/main/java/team/snowball/baseball/model/Stadium.java
@@ -0,0 +1,23 @@
+package team.snowball.baseball.model;
+
+import lombok.*;
+import java.sql.Timestamp;
+
+/**
+ * author         : Yongwon Kim
+ * date           : 2023-06-26
+ * description    :
+ */
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Stadium {
+
+    private Long id;
+    private String name;
+    private Timestamp createdA;
+
+}
