393cd6ae7b681a53dc1577cbc93083dc7598284a
Horaiz-UQ <ekzmvlfxj@naver.com>
2023-06-26 17:51:34 +0900

    Implement #11 - Stadium DAO 생성
    * Stadium DAO 구현

--- src/main/java/team/snowball/baseball/dao/StadiumDao.java
+++ src/main/java/team/snowball/baseball/dao/StadiumDao.java
@@ -1,11 +1,18 @@
 package team.snowball.baseball.dao;
 
 import lombok.Getter;
+import team.snowball.baseball.handler.DatabaseException;
 import team.snowball.baseball.model.Stadium;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+
+/**
+ * author         : Yongwon Kim
+ * date           : 2023-06-26
+ * description    :
+ */
 
 @Getter
 public class StadiumDao {
@@ -17,20 +24,34 @@ public class StadiumDao {
     }
 
     // 야구장 등록
-    public void createStadium(Stadium stadium) throws SQLException {
+    public void createStadium(Stadium stadium) {
         String query = "INSERT INTO stadium(name, created_at) VALUES (?, now())";
-        try (PreparedStatement statement = connection.prepareStatement(query)) {
+        PreparedStatement statement = null;
+        try {
+            statement = connection.prepareStatement(query);
             statement.setString(1, stadium.getName());
             statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DatabaseException(e.getMessage());
+        } finally {
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException e) {
+                    throw new DatabaseException(e.getMessage());
+                }
+            }
+            SnowballDBManager.disconnect(connection, null, null);
         }
     }
 
     // 전체 야구장 조회
-    public List<Stadium> getAllStadiums() throws SQLException {
+    public List<Stadium> getAllStadiums() {
         List<Stadium> stadiums = new ArrayList<>();
         String query = "SELECT * FROM stadium";
-        try (Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery(query)) {
+        ResultSet resultSet = null;
+        try (Statement statement = connection.createStatement()) {
+            resultSet = statement.executeQuery(query);
             while (resultSet.next()) {
                 Long id = resultSet.getLong("id");
                 String name = resultSet.getString("name");
@@ -38,9 +59,20 @@ public class StadiumDao {
                 Stadium stadium = new Stadium(id, name, createdA);
                 stadiums.add(stadium);
             }
+        } catch (SQLException e) {
+            throw new DatabaseException(e.getMessage());
+        } finally {
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {
+                    throw new DatabaseException(e.getMessage());
+                }
+            }
+            SnowballDBManager.disconnect(connection, null, null);
         }
         return stadiums;
     }
 
-
 }
+
