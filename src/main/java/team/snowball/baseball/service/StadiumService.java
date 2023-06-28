fea4206e3d614b1eabf65f8c10149f111ceccb0e
Horaiz-UQ <ekzmvlfxj@naver.com>
2023-06-28 14:16:04 +0900

    Refactor #18 - StadiumService 수정 * CommandService 추가

--- src/main/java/team/snowball/baseball/service/StadiumService.java
+++ src/main/java/team/snowball/baseball/service/StadiumService.java
@@ -1,31 +1,54 @@
 package team.snowball.baseball.service;
 
 import team.snowball.baseball.dao.StadiumDao;
-import team.snowball.baseball.model.Stadium;
+import team.snowball.baseball.model.stadium.Stadium;
+
 import java.util.List;
 
 /**
  * author         : Yongwon Kim
- * date           : 2023-06-27
+ * date           : 2023-06-28
  * description    :
  */
+public class StadiumService implements CommandService {
 
-public class StadiumService {
-    private StadiumDao stadiumDao;
+    private final StadiumDao stadiumDao;
 
-    public StadiumService(StadiumDao stadiumDao) {
-        this.stadiumDao = stadiumDao;
+    public static StadiumService stadiumService;
+
+    private StadiumService() {
+        stadiumDao = new StadiumDao();
     }
 
-    // 야구장 생성 로직 구현
-    public void createStadium(String name) {
-        Stadium stadium = new Stadium(null, name, null);
-        stadiumDao.createStadium(stadium);
+    public static StadiumService getInstance() {
+        if (stadiumService == null) {
+            stadiumService = new StadiumService();
+        }
+        return stadiumService;
     }
 
-    // 야구장 전체 목록 로직 구현
-    public List<Stadium> getAllStadiums() {
-        return stadiumDao.getAllStadiums();
+    @Override
+    public void create() {
+        Stadium stadium = new Stadium();
+        stadiumDao.insert(stadium);
+    }
+
+    @Override
+    public void read() {
+        List<Stadium> stadiums = stadiumDao.findAllStadiums();
+        for (Stadium stadium : stadiums) {
+            System.out.println(stadium);
+        }
+    }
+
+    @Override
+    public void update() {
+        System.out.println("수정");
+    }
+
+    @Override
+    public void delete() {
+        System.out.println("삭제");
     }
 
 }
