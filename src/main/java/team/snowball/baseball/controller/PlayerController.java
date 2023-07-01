4e24a93a6c8e99873b4e57d822361e5d05e1517d
matrixpower1004 <matrixpower1004@gmail.com>
2023-07-01 21:13:56 +0900

    HOXFIX #47 - contrller 로직 버그 수정
    * 컨트롤러 로직의 버그에 의한 Exception 발생을 수정

--- src/main/java/team/snowball/baseball/controller/PlayerController.java
+++ src/main/java/team/snowball/baseball/controller/PlayerController.java
@@ -1,15 +1,16 @@
 package team.snowball.baseball.controller;
 
 import team.snowball.baseball.code.Command;
-import team.snowball.baseball.code.Domain;
 import team.snowball.baseball.dto.QueryParseDto;
 import team.snowball.baseball.handler.InvalidInputException;
 import team.snowball.baseball.model.player.Player;
 import team.snowball.baseball.service.PlayerService;
 
 import java.util.Map;
+import java.util.function.Consumer;
 import java.util.function.Function;
 
+import static team.snowball.baseball.code.Domain.POSITION_BY;
 import static team.snowball.baseball.code.ErrorMessage.ERR_MSG_INVALID_PARAMETER;
 
 /**
@@ -34,30 +35,38 @@ public class PlayerController implements ModelController {
 
     @Override
     public void execute(QueryParseDto queryParseDto) {
-        // 예외적인 명령인 포지션별목록을 처리
-        if (queryParseDto.getDomain().equals(Domain.POSITION_BY)
-                && queryParseDto.getCommand().equals(Command.READ)) {
-            playerService.positionByReport();
-        }
-        if (queryParseDto.getCommand().equals(Command.CREATE)) {
-            Player player = getPlayerParams.apply(queryParseDto);
-            playerService.create(player);
-        }
-        if (queryParseDto.getCommand().equals(Command.READ)) {
-            Player player = getPlayerParams.apply(queryParseDto);
-            int teamId = player.getTeamId();
-            System.out.println(teamId);
-            playerService.read(teamId);
-        }
-        if (queryParseDto.getCommand().equals(Command.PUT)) {
-            Player player = getPlayerParams.apply(queryParseDto);
-            playerService.update(player);
-        }
-        if (queryParseDto.getCommand().equals(Command.DELETE)) {
-            Long id = getParamId.apply(queryParseDto);
-            playerService.delete(id);
+        Command command = queryParseDto.getCommand();
+
+        switch (command) {
+            case CREATE -> playerService.create(getPlayerParams.apply(queryParseDto));
+            /**
+             * READ 에는 3가지 경우가 존재
+             * 1. 특이 케이스인 '포지션별목록' 명령어를 처리하는 경우
+             * 2. ?teamdId=1과 같이 특정 팀의 선수 목록을 출력하는 경우
+             * 3. 옵션 없이 전체 선수 목록을 출력하는 경우
+             */
+            case READ -> selectPlayerRead.accept(queryParseDto);
+            case PUT -> playerService.update(getPlayerParams.apply(queryParseDto));
+            case DELETE -> playerService.delete(getParamId.apply(queryParseDto));
+            default -> throw new InvalidInputException(ERR_MSG_INVALID_PARAMETER.getErrorMessage());
         }
     }
+
+    private Consumer<QueryParseDto> selectPlayerRead = (queryParseDto) -> {
+        // 1. TeamId로 조회하는 경우
+        if (queryParseDto.getParams().containsKey("teamId")) {
+            Long id = getParamId.apply(queryParseDto);
+            playerService.read(id);
+            return;
+        }
+        // 2. 포지션변목록 보기
+        if (queryParseDto.getDomain().equals(POSITION_BY)) {
+            playerService.readPositionBy();
+            return;
+        }
+        // 전체 선수 목록 보기
+        playerService.read();
+    };
 
     private Function<QueryParseDto, Player> getPlayerParams = (queryParseDto) -> {
         String teamId = "";
@@ -76,12 +85,12 @@ public class PlayerController implements ModelController {
                     position = entry.getValue();
                 }
             }
-        } catch (IllegalStateException e) {
+        } catch (IllegalStateException | NullPointerException e) {
             throw new InvalidInputException(ERR_MSG_INVALID_PARAMETER.getErrorMessage());
         }
 
         Player player = Player.builder()
-                .teamId(Integer.valueOf(teamId))
+                .teamId(Long.valueOf(teamId))
                 .name(name)
                 .position(position)
                 .build();
